e04759a51eb72d6cb12655f272e33170e6bf87e5
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2024-10-02 02:25:39 +0500

    Task7

---
+++ Tyuiu.BilousEYu.Sprint1.Task7.V2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.BilousEYu.Sprint1.Task7.V2.Lib;
+namespace Tyuiu.BilousEYu.Sprint1.Task7.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Билоус Е. Ю. | ИСТНб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Билоус Елизавета Юрьевна | ИСТНб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("    sinx + cosy");
+            Console.WriteLine("z = ----------- tgxy");
+            Console.WriteLine("    cosx - siny");
+            Console.WriteLine("Введите x: ");
+            double x;
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y: ");
+            double y;
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
